5603bd297f4bfe93a0d6b615ad95a80bf33933ad
Polo1dev <dalain@buyblack365.com>
2016-07-20 19:48:02 -0500

    Starter template project using C# ASP.NET MVC.  and WebAPI. Use, Expand, Destroy...

---
+++ Sample_App/Sample_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sample_App.Startup))]
+namespace Sample_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
